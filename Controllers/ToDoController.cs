60fa8313dd0ab96b742a25aa8e09f922cb96c8c4
vanessabiscos <biscos_vanessa@yahoo.com>
2022-02-16 20:10:39 +0200

    Initial commit

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Biscos_Vanessa_ToDoList.Infrastructure;
+using Biscos_Vanessa_ToDoList.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biscos_Vanessa_ToDoList.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private readonly ToDoContext context;
+
+        public ToDoController(ToDoContext context)
+        {
+            this.context = context;
+        } //We can use the database to show on the site
+
+        // GET /
+        public async Task<ActionResult> Index()
+        {
+            IQueryable<TodoList> items = from i in context.ToDoList orderby i.Id select i;
+
+            List<TodoList> todoLists = await items.ToListAsync();
+
+            return View(todoLists);
+        }
+
+        // GET /todo/create
+        public IActionResult Create() => View();
+
+        //POST /todo/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(TodoList item)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(item);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The item has been added with success!";
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+
+        // GET /todo/edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            TodoList item = await context.ToDoList.FindAsync(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        //POST /todo/edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(TodoList item)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Update(item);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The item has been updated with success!";
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+
+        // GET /todo/delete/5-id of the item
+        public async Task<ActionResult> Delete(int id)
+        {
+            TodoList item = await context.ToDoList.FindAsync(id);
+
+            if (item == null)
+            {
+                TempData["Error"] = "The item does not exist!";
+                return NotFound();
+            }
+            else
+            {
+                context.ToDoList.Remove(item);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The item has been deleted with success!";
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        
+    }
+}
---
+++ Infrastructure/ToDoContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Biscos_Vanessa_ToDoList.Models;
+
+
+namespace Biscos_Vanessa_ToDoList.Infrastructure
+{
+    public class ToDoContext : DbContext
+
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<TodoList> ToDoList { get; set; }
+    
+    }
+}
